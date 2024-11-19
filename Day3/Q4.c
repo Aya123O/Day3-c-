bcb175db7a3764bc8fa1f3fd4452a9d2a50cbcb7
Aya Osama <109994117+Aya123O@users.noreply.github.com>
2024-11-19 12:57:16 +0200

    Update Q4.c

--- Day3/Q4.c
+++ Day3/Q4.c
@@ -13,21 +13,23 @@ int main() {
 
 
     int arr[100];
+     for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d\t", arr[i * cols + j]);
+        }
+        printf("\n");
+    }
+    
 
 
     printf("Enter the elements of the array:\n");
     for (int i = 0; i < rows * cols; i++) {
-        scanf("%d", &arr[i]);
+        printf("%d", &arr[i]);
     }
 
 
     printf("\nArray displayed as a table:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d\t", arr[i * cols + j]);
-        }
-        printf("\n");
-    }
+   
 
     return 0;
 }
